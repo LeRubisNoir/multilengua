3fd838e856dea96a87dbcc7450c9869152523efd
Yoldark34 <yoldark@gmail.com>
2014-04-16 13:41:41 +0200

    step contact

--- features/step_definitions/contact_page_step.rb
+++ features/step_definitions/contact_page_step.rb
@@ -1,20 +1,23 @@
 Given(/^I am on the Contact page$/) do
-  pending # express the regexp above with the code you wish you had
+  visit contact_path
 end
 
-Then(/^I should see title "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^I should see title "(.*?)"$/) do |title|
+  expect(contact_path).to have_selector "h1", text: title
 end
 
 Given(/^I have the menu opened$/) do
-  pending # express the regexp above with the code you wish you had
+  pending #Check if the menu is open
 end
 
 When(/^I click contact icon$/) do
-  pending # express the regexp above with the code you wish you had
+  within("#contact_icon") do
+    find(:link, "Contact").click
+  end
 end
 
 Then(/^I should change page to Contact$/) do
-  pending # express the regexp above with the code you wish you had
+  request.request_uri.should == send("contacts_path")
+  response.should be_success
 end
 
