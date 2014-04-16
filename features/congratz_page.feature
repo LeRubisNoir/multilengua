c54e04d10bd8a39270a8fb051b9df7704a3453b0
Yoldark34 <yoldark@gmail.com>
2014-04-16 14:40:42 +0200

    modification des features

--- features/congratz_page.feature
+++ features/congratz_page.feature
@@ -3,14 +3,9 @@ Feature: Access congratz
   As an everybody
   I should finish the exercise
 
-  Scenario: Going to Congratz page
-  Given I have finished the exercise
-  When I click on the next button
-  Then I should change page to Congratz
-
   Scenario: Be on Congratz
   Given I am on the Congratz page
   Then I should see title "Congratz" in the selector "h1"
   Then I should see text "Your score :" in the selector "p"
-  Then I should see the button "Did you like it?? \n To continue just login !" in the selector "p" with the class "continue_paragraph"
-  Then I should see the button "Log in" in the selector "button" with the class "login_button"
+  Then I should see the text "Did you like it?? To continue just login !" in the selector "p" with the class "continue_paragraph"
+  Then I should see the button with the text "Log in" in the selector "button" with the class "login_button"
--- features/step_definitions/congratz_page_step.rb
+++ features/step_definitions/congratz_page_step.rb
@@ -1,24 +1,20 @@
-Given(/^I have finished the exercise$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-When(/^I click on the next button$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^I should change page to Congratz$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
 Given(/^I am on the Congratz page$/) do
   pending # express the regexp above with the code you wish you had
 end
 
+Then(/^I should see title "(.*?)" in the selector "(.*?)"$/) do |title, selector|
+  expect(congratz_path).to have_selector selector, text: title
+end
+
 Then(/^I should see text "(.*?)" in the selector "(.*?)"$/) do |text, selector|
+  expect(congratz_path).to have_selector selector, text: text
+end
+
+Then(/^I should see the text "(.*?)" in the selector "(.*?)" with the class "(.*?)"$/) do |text, selector, class_name|
   pending # express the regexp above with the code you wish you had
 end
 
-Then(/^I should see the button "(.*?)" in the selector "(.*?)" with the class "(.*?)"$/) do |text, selector, class_name|
+Then(/^I should see the button with the text "(.*?)" in the selector "(.*?)" with the class "(.*?)"$/) do |text, selector, class_name|
   pending # express the regexp above with the code you wish you had
 end
 
