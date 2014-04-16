4c1f6b59fd3ab24fdc775aa73ed966a78c360697
Yoldark34 <yoldark@gmail.com>
2014-04-16 11:53:22 +0200

    Add Scenario to test being on contact page

--- features/contact_page.feature
+++ features/contact_page.feature
@@ -3,6 +3,10 @@ Feature: Acess contact page
   As an everybody
   I want to reach the selected page
 
+  Scenario: Be on Contact
+  Given I am on the Contact page
+  Then I should see title "Contact"
+
   Scenario: Select Contact
   Given I have the menu opened
   When I click contact icon
--- features/step_definitions/contact_page_step.rb
+++ features/step_definitions/contact_page_step.rb
@@ -1,3 +1,11 @@
+Given(/^I am on the Contact page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see title "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
 Given(/^I have the menu opened$/) do
   pending # express the regexp above with the code you wish you had
 end
